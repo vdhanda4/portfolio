1d4c4c23ce979e67d9c9107c3b71a7783e722245
vdhanda4 <vdhanda@ucsd.edu>
2025-04-25 22:57:21 -0700

    lab 3

--- global.js
+++ global.js
@@ -3,3 +3,65 @@ console.log("IT'S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/"; 
+
+const pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "resume/", title: "Resume" },
+  { url: "contact/", title: "Contact" },
+  { url: "https://github.com/vdhanda4", title: "GitHub" },
+];
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  if (!url.startsWith("http")) {
+    url = BASE_PATH + url;
+  }
+  let a = document.createElement('a');
+a.href = url;
+a.textContent = title;
+
+
+if (a.host === location.host && a.pathname === location.pathname) {
+  a.classList.add('current');
+}
+if (a.host !== location.host) {
+  a.target = "_blank";
+}
+
+nav.append(a)
+}
+document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <label class="color-scheme">
+      Theme:
+      <select id="theme-select">
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+  `
+  );
+  let select = document.querySelector("#theme-select");
+
+
+if ("colorScheme" in localStorage) {
+  let saved = localStorage.colorScheme;
+  document.documentElement.style.setProperty("color-scheme", saved);
+  select.value = saved; 
+}
+select.addEventListener("input", function (event) {
+  document.documentElement.style.setProperty("color-scheme", event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+  
